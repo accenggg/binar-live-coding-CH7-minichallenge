2d9ef91b1e9362cbeab547db1271a5b4d6273eae
accenggg <arseliusngiklan@gmail.com>
2023-11-15 15:56:38 +0800

    first commit

--- tests/Car.api.test.js
+++ tests/Car.api.test.js
@@ -64,6 +64,61 @@ describe("API create car", () => {
   });
 });
 
+describe("API rent car By ID", () => {
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6IkZpa3JpIiwiZW1haWwiOiJmaWtyaUBiaW5hci5jby5pZCIsImltYWdlIjpudWxsLCJyb2xlIjp7ImlkIjoxLCJuYW1lIjoiQ1VTVE9NRVIifSwiaWF0IjoxNzAwMDIyODcxfQ.3rKv8AoAI7j_yfTFjz3GlofhchJbthIci5J3HtVDOMQ";
+
+  const rentStartedAt = new Date();
+
+  it("success rent car", async () => {
+    const response = await request(app)
+      .post("/v1/cars/1/rent")
+      .send({
+        rentStartedAt,
+      })
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.statusCode).toBe(201);
+  });
+
+  // it("car is currently rent", async () => {
+  //   const response = await request(app)
+  //     .post("/v1/cars/2/rent")
+  //     .send({
+  //       rentStartedAt,
+  //     })
+  //     .set("Authorization", `Bearer ${token}`);
+  //   expect(response.statusCode).toBe(422);
+  // });
+});
+
+describe("API update car", () => {
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IkpvaG5ueSIsImVtYWlsIjoiam9obm55QGJpbmFyLmNvLmlkIiwiaW1hZ2UiOm51bGwsInJvbGUiOnsiaWQiOjIsIm5hbWUiOiJBRE1JTiJ9LCJpYXQiOjE3MDAwMzQwNjF9.gWbb25WUBtuzrtNpImjmLNQGzukQIMA1spK1EM5ipks";
+
+  it("success update car", async () => {
+    const response = await request(app)
+      .put("/v1/cars/1")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        name: "Suzuki",
+        price: 300000,
+        size: "SMALL",
+        image: `https://https://unsplash.com/photos/a-group-of-antelope-standing-in-the-desert-i60yUhfWeYI`,
+      });
+    expect(response.statusCode).toBe(200);
+  });
+
+  // it("failed update car", async () => {
+  //   const response = await request(app).put("/v1/cars/1").send({
+  //     name: "Suzuki",
+  //     price: 300000,
+  //     size: "SMALL",
+  //     image: `https://https://unsplash.com/photos/a-group-of-antelope-standing-in-the-desert-i60yUhfWeYI`,
+  //   });
+  //   expect(response.statusCode).toBe(422);
+  // });
+});
+
 // describe("API failed create car without req.body", () => {
 //   const token =
 //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwibmFtZSI6IkpvaG5ueSIsImVtYWlsIjoiam9obm55QGJpbmFyLmNvLmlkIiwiaW1hZ2UiOm51bGwsInJvbGUiOnsiaWQiOjIsIm5hbWUiOiJBRE1JTiJ9LCJpYXQiOjE2OTk4ODU1NDF9.ZMTs6GtxtXjixTa-s-ok2JQg--HwD4k6W_gfNHYqQUQ";
